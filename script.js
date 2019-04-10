fe2dec76e48f7fbf27d2162b3fd51ab55dbdc543
malindarivers <43344616+malindarivers@users.noreply.github.com>
2019-04-10 09:42:27 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,39 @@
+//Try to solve all the challenges below--
+//Challenge 1: Fix the game so that the buttons reappear. 
+//Challenge 2: Make a new button that uses jQuery to add the following text to the screen: "The solution is 4,2,6,7,3,5,1."
+//Challenge 3: Use jQuery to make the buttons bigger. 
+//Challenge 4: Make all the buttons different colors using CSS.
+//Challenge 5: Use jQuery to change the background color of the screen every time you click a new button. 
+//Challenge 6: Center the instructions on the page. 
+//Challenge 7: Make a new button that refreshes all the buttons.
+
+//Export your code as a gist before doing the last challenge!
+
+//Challenge 8: Look at the HTML section of the code and find the secret hidden link. Use jQuery to make it visible, then follow the link to the secret challenge page!
+
+
+        $("#one").click(function(){
+            $("#one").show();
+        });
+        $("#two").click(function(){
+            $("#two").hide();
+        });
+        $("#three").click(function(){
+            $("#three").hide();
+        });
+        $("#four").click(function(){
+            $("#four").hide();
+        });
+        $("#five").click(function(){
+            $("#five").hide();
+        });
+        $("#six").click(function(){
+            $("#six").hide();
+        });
+        $("#seven").click(function(){
+            $("#seven").hide();
+        });
+        $("#eight").click(function(){
+            $("#eight").hide();
+        });
+        
